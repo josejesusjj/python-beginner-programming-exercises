4ff779ca2a551e57ab632ca297f79ad319bc943b
Jose <josejesuscedeno@gmail.com>
2022-03-02 16:22:54 +0000

    08.1 wedding costs

--- exercises/02-Declare-Variables/app.py
+++ exercises/02-Declare-Variables/app.py
@@ -1,1 +1,3 @@
 # your code here
+yellow = "Yellow"
+print(yellow)
--- exercises/08-Your-First-If/app.py
+++ exercises/08-Your-First-If/app.py
@@ -1,4 +1,6 @@
 
 total = int(input('How much money do you have in your pocket\n'))
-
 # YOUR CODE HERE
+if total <= 50: print("You are a poor guy, go away!")
+elif total <=100: print("Buy me some coffee you cheap!")
+else: print("Give me your money!")
