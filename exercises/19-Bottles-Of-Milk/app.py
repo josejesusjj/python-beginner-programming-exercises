8b765ad955f47c8634790e6558466b60a893f65d
Jose <josejesuscedeno@gmail.com>
2022-07-06 14:48:14 +0000

    funciona el bottle of milk, probado en otro environment

--- exercises/19-Bottles-Of-Milk/app.py
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -1,33 +1,10 @@
-"""Ninety-Nine Bottles of Milk on the Wall
- By Al Sweigart al@inventwithpython.com
- Print the full lyrics to one of the longest songs ever! Press
- Ctrl-C to stop.
- This code is available at https://nostarch.com/big-book-small-python-programming
- Tags: tiny, beginner, scrolling"""
  
+def number_of_bottles():
+
+    for x in range(99,-1,-1):  
+        if x == 1: print(str(x) + " bottle of milk on the wall, " + str(x) + " bottle of milk.\nTake one down and pass it around, no more bottles of milk on the wall.")
+        elif x == 0: print("No more bottles of milk on the wall, no more bottles of milk.\nGo to the store and buy some more, 99 bottles of milk on the wall.")
+        else: print(str(x) + " bottles of milk on the wall, " + str(x) + " bottles of milk.\nTake one down and pass it around, " + str(x-1) + " bottles of milk on the wall.")
 
 
-bottles = 99  # This is the starting number of bottles.
-PAUSE = 2  # (!) Try changing this to 0 to see the full song at once.
- 
-try:
-     while bottles > 1:  # Keep looping and display the lyrics.
-         print(bottles, 'bottles of milk on the wall,')
-
-         print(bottles, 'bottles of milk,')
-
-         print('Take one down, pass it around,')
- 
-         bottles = bottles - 1  # Decrease the number of bottles by one.
-         print(bottles, 'bottles of milk on the wall!')
-
-         print()  # Print a newline.
- 
-     # Display the last stanza:
-     print('1 bottle of milk on the wall,')
-
-     print('1 bottle of milk,')
-
-     print('Take it down, pass it around,')
-
-     print('No more bottles of milk on the wall!')
+number_of_bottles()
